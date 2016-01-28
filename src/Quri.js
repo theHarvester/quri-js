0bee1d53729a4d8cbc03894b6c24f8b1c8ece6d9
Tim van der Horst <tim.vanderhorst@smg-technologies.com>
2016-01-28 14:10:14 +1000

    - Rename index files to their own individual files - Converted eslintrc to YAML format - Added EditorConfig settings

---
+++ src/Quri.js
@@ -0,0 +1,112 @@
+/**
+ * @class Criteria
+ *
+ * A class to wrap a QURI string creation
+ */
+export default class Criteria {
+  /**
+   * Criteria constructor
+   *
+   * @param andOr Sets the and|or for all expressions at this level
+   */
+  constructor(andOr = 'and') {
+    this.andOr = andOr;
+    this.criteria = [];
+  }
+
+  /**
+   * Appends a new expression.
+   *
+   * @example appendExpression('customer_name', 'like', 'Greg%')
+   *
+   * @param {string} fieldName Field name
+   * @param {string} operator String '==', '!=', '>', '>=', '<', '<=', 'in', 'not_in', 'like', 'between'
+   * @param {string,number,Array} value Value(s) that match the operator.
+   * The only operators that allow array values are in|out|nin
+   * @returns {Criteria}
+   */
+  appendExpression(fieldName, operator, value) {
+    this.criteria.push({
+      fieldName: fieldName,
+      operator: operator,
+      value: value
+    });
+    return this;
+  }
+
+  /**
+   * Appends a criteria object to the current criteria array.
+   * The criteria object may be a new Criteria class or any object with a toString method.
+   *
+   * @param {Criteria} criteria
+   * @returns {Criteria}
+   */
+  appendCriteria(criteria) {
+    this.criteria.push({
+      criteria: criteria
+    });
+    return this;
+  }
+
+  /**
+   * Returns the formatted QURI string
+   *
+   * @returns {string}
+   */
+  toString() {
+    let criteriaMap = this.criteria.map(expression => {
+      if (expression.hasOwnProperty('criteria')) {
+        return '(' + expression.criteria.toString() + ')';
+      }
+      let operator = Criteria.operatorToString(expression.operator);
+      let valueStr = JSON.stringify(expression.value);
+      if (valueStr.charAt(0) == '[') {
+        // If it's an array we need to remove the [ ] from the outside.
+        valueStr = valueStr.substring(1, valueStr.length - 1);
+      }
+      return JSON.stringify(expression.fieldName) + '.' + operator + '(' + valueStr + ')';
+    });
+    return criteriaMap.join(this.andOr == 'and' ? ',' : '|');
+  }
+
+  /**
+   * Takes the operator string from the user and returns the corresponding string in the QURI specificiation.
+   *
+   * @example operatorToString('=='); // 'eq'
+   *
+   * @param {string} operatorStr
+   * @returns {string}
+   */
+  static operatorToString(operatorStr) {
+    switch (operatorStr) {
+      case '=':
+      case '==':
+      case 'eq':
+        return 'eq';
+      case '!=':
+      case 'neq':
+        return 'neq';
+      case '>':
+      case 'gt':
+        return 'gt';
+      case '>=':
+      case 'gte':
+        return 'gte';
+      case '<':
+      case 'lt':
+        return 'lt';
+      case '<=':
+      case 'lte':
+        return 'lte';
+      case 'in':
+        return 'in';
+      case 'not_in':
+      case 'nin':
+        return 'nin';
+      case 'like':
+        return 'like';
+      case 'between':
+        return 'between';
+    }
+  }
+}
---
+++ test/Quri.spec.js
@@ -0,0 +1,49 @@
+import test from "tape";
+import Criteria from "../src/Quri.js";
+
+test("test basic string can be created from criteria", (t) => {
+  t.plan(1);
+  let criteria = new Criteria();
+  criteria.appendExpression('field_1', '=', 'my value');
+  t.equal('"field_1".eq("my value")', criteria.toString());
+});
+
+test("test closure is applied to inner criteria", (t) => {
+  t.plan(1);
+  let criteria = new Criteria();
+  criteria.appendExpression('field_1', '=', 'my value');
+  let criteriaInner = new Criteria('or');
+  criteriaInner.appendExpression('field_2', '=', 'my inner value');
+  criteriaInner.appendExpression('field_3', '=', 'my inner value 2');
+  criteria.appendCriteria(criteriaInner);
+  t.equal('"field_1".eq("my value"),("field_2".eq("my inner value")|"field_3".eq("my inner value 2"))', criteria.toString(), "is equal");
+});
+
+test("test quote escaping works correctly", (t) => {
+  t.plan(1);
+  let criteria = new Criteria();
+  criteria.appendExpression('field"1', '=', 'my"value');
+  t.equal('"field\\"1".eq("my\\"value")', criteria.toString());
+});
+
+test("test arrays are stringified correctly", (t) => {
+  t.plan(1);
+  let criteria = new Criteria();
+  criteria.appendExpression('field_1', 'not_in', [1,2,3,4]);
+  t.equal('"field_1".nin(1,2,3,4)', criteria.toString());
+});
+
+test("test supported operator strings", (t) => {
+  t.equal('eq', Criteria.operatorToString('=='));
+  t.equal('neq', Criteria.operatorToString('!='));
+  t.equal('gt', Criteria.operatorToString('>'));
+  t.equal('gte', Criteria.operatorToString('>='));
+  t.equal('lt', Criteria.operatorToString('<'));
+  t.equal('lte', Criteria.operatorToString('<='));
+  t.equal('in', Criteria.operatorToString('in'));
+  t.equal('nin', Criteria.operatorToString('not_in'));
+  t.equal('nin', Criteria.operatorToString('not_in'));
+  t.equal('like', Criteria.operatorToString('like'));
+  t.equal('between', Criteria.operatorToString('between'));
+  t.end();
+});
